720e733ffa65db9de29edaf287f7282ccfad48a2
Raimund Wege <raimundwege@Raimunds-MBP.home>
2020-02-01 13:37:34 +0100

    Added trufi core dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,142 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:global_configuration/global_configuration.dart';
+import 'package:latlong/latlong.dart';
+import 'package:trufi_core/trufi_configuration.dart';
+import 'package:trufi_core/trufi_app.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+  final trufiCfg = TrufiConfiguration();
+  final globalCfg = GlobalConfiguration();
+  await globalCfg.loadFromAsset("app_config");
+
+  // Abbreviations
+  trufiCfg.abbreviations.addAll({
+    "Avenida": "Av.",
+    "Calle": "C.",
+    "Camino": "C.º",
+  });
+
+  // Animation
+  trufiCfg.animation.loading = FlareActor(
+    "assets/images/loading.flr",
+    animation: "Trufi Drive",
+  );
+  trufiCfg.animation.success = FlareActor(
+    "assets/images/success.flr",
+    animation: "Untitled",
+  );
+
+  // Attribution
+  trufiCfg.attribution.representatives.addAll([
+    "Christoph Hanser",
+    "Samuel Rioja",
+  ]);
+  trufiCfg.attribution.team.addAll([
+    "Andreas Helms",
+    "Annika Bock",
+    "Christian Brückner",
+    "Javier Rocha",
+    "Luz Choque",
+    "Malte Dölker",
+    "Martin Kleppe",
+    "Michael Brückner",
+    "Natalya Blanco",
+    "Neyda Mili",
+    "Raimund Wege",
+  ]);
+  trufiCfg.attribution.translations.addAll([
+    "Gladys Aguilar",
+    "Jeremy Maes",
+    "Gaia Vitali Roscini",
+  ]);
+  trufiCfg.attribution.routes.addAll([
+    "Trufi team",
+    "Guia Cochala team",
+  ]);
+  trufiCfg.attribution.osm.addAll([
+    "Marco Antonio",
+    "Noémie",
+    "Philipp",
+    "Felix D",
+    "Valor Naram", // Sören Reinecke
+  ]);
+
+  // Email
+  trufiCfg.email.feedback = globalCfg.getString("emailFeedback");
+  trufiCfg.email.info = globalCfg.getString("emailInfo");
+
+  // Image
+  trufiCfg.image.drawerBackground = "assets/images/drawer-bg.jpg";
+
+  // Map
+  trufiCfg.map.mapTilerKey = globalCfg.get("keyMapTiler");
+  trufiCfg.map.defaultZoom = 12.0;
+  trufiCfg.map.offlineMinZoom = 8.0;
+  trufiCfg.map.offlineMaxZoom = 14.0;
+  trufiCfg.map.offlineZoom = 13.0;
+  trufiCfg.map.onlineMinZoom = 1.0;
+  trufiCfg.map.onlineMaxZoom = 19.0;
+  trufiCfg.map.onlineZoom = 13.0;
+  trufiCfg.map.chooseLocationZoom = 16.0;
+  trufiCfg.map.center = LatLng(5.574558, -0.214656);
+  trufiCfg.map.southWest = LatLng(5.510057, -0.328217);
+  trufiCfg.map.northEast = LatLng(5.726678, 0.071411);
+
+  // Languages
+  trufiCfg.languages.addAll([
+    TrufiConfigurationLanguage(
+      languageCode: "de",
+      countryCode: "DE",
+      displayName: "Deutsch",
+    ),
+    TrufiConfigurationLanguage(
+      languageCode: "en",
+      countryCode: "US",
+      displayName: "English",
+    ),
+    TrufiConfigurationLanguage(
+      languageCode: "es",
+      countryCode: "ES",
+      displayName: "Español",
+      isDefault: true,
+    ),
+    TrufiConfigurationLanguage(
+      languageCode: "fr",
+      countryCode: "FR",
+      displayName: "Français",
+    ),
+    TrufiConfigurationLanguage(
+      languageCode: "it",
+      countryCode: "IT",
+      displayName: "Italiano",
+    ),
+    TrufiConfigurationLanguage(
+      languageCode: "qu",
+      countryCode: "BO",
+      displayName: "Quechua simi",
+    ),
+  ]);
+
+  // Url
+  trufiCfg.url.otpEndpoint = globalCfg.getString("urlOtpEndpoint");
+  trufiCfg.url.routeFeedback = globalCfg.getString("urlRouteFeedback");
+  trufiCfg.url.donate = globalCfg.getString("urlDonate");
+  trufiCfg.url.website = globalCfg.getString("urlWebsite");
+  trufiCfg.url.facebook = globalCfg.getString("urlFacebook");
+  trufiCfg.url.twitter = globalCfg.getString("urlTwitter");
+
+  // Run app
+  runApp(
+    TrufiApp(
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xff263238),
+        accentColor: const Color(0xffc01100),
+        backgroundColor: Colors.white,
+        primaryColorLight: const Color(0xffeceff1),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    ),
+  );
 }
