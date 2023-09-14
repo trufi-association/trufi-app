f95616eeea91e0ec6bfb3d3c781834204b89beb1
Gustavo T <tgustavoch@gmail.com>
2023-09-14 17:11:44 -0400

    [Feat] Add TrufiAppLocalization translations

---
+++ lib/translations/trufi_app_localizations_en.dart
@@ -0,0 +1,135 @@
+import 'trufi_app_localizations.dart';
+
+/// The translations for English (`en`).
+class TrufiAppLocalizationEn extends TrufiAppLocalization {
+  TrufiAppLocalizationEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get menuFaresGuidelines => 'Fares & Guidelines';
+
+  @override
+  String get guidelinesTypePassengerOlder => 'Older';
+
+  @override
+  String get guidelinesTypePassengerSeniorCitizen => 'Senior citizen';
+
+  @override
+  String get guidelinesTypePassengerDisabled => 'Disabled';
+
+  @override
+  String get guidelinesTypePassengerUniversityStudents => 'University students';
+
+  @override
+  String get guidelinesTypePassengerPrimarySecondaryStudents => 'Primary and secondary students';
+
+  @override
+  String get guidelinesTypePassengerMeritorious => 'Meritorious';
+
+  @override
+  String get guidelinesTypePassengerChildrenUnderFive => 'Children under 5 years old';
+
+  @override
+  String get guidelinesTypePassengerStudents => 'Students';
+
+  @override
+  String get guidelinesExemptPay => 'Exempt from payment';
+
+  @override
+  String get guidelinesIntroduction => 'Here you will find important information and useful tips to help you make the most of your public transportation experience.';
+
+  @override
+  String get guidelinesClarification => 'It is important to note that since public transportation in Cochabamba is mainly informal and does not have fixed schedules. The application provides data based on pre-established schedules and routes, which may have variations in arrival times and routes.';
+
+  @override
+  String get guidelinesFareCost => 'Fare Cost';
+
+  @override
+  String get guidelinesTransitFare => 'Trufis, Micros and Minibuses Fare';
+
+  @override
+  String get guidelinesTransitFareIntroduction => 'According to Municipal Decree No. 003/2014, you have the following fare schedule:';
+
+  @override
+  String get guidelinesTransitFareClarification => 'However, on some occasions, this fare is not respected and is rounded up to 2 Bs.';
+
+  @override
+  String get guidelinesMetropolitanFare => 'Fare for the metropolitan axis';
+
+  @override
+  String get guidelinesMetropolitanFareIntroduction => 'For interprovincial sections of the metropolitan axis, the rates established by Departmental Decree 1399 apply:';
+
+  @override
+  String get guidelinesMetropolitanFareClarification => 'Exceptions: As of 10:00 pm the night schedule is in effect, with an increase of Bs 0.50 in both the urban and interprovincial axis. In some cases, the fare also varies depending on the section of the route, increasing or decreasing by Bs 0.50 as detailed in the tables. In any case, if you are not sure, you can ask the driver or another passenger.';
+
+  @override
+  String get guidelinesPublicTransportation => 'How to use public transportation';
+
+  @override
+  String get guidelinesPublicTransportationDescription => 'If you go by trufi, you pay when you get off and when you get on if it\'s a coaster. You can get off or stop wherever you want but try to do it when the traffic light is red to avoid congestion, and let the driver know  in advance.';
+
+  @override
+  String get guidelinesStops => 'Stops';
+
+  @override
+  String get guidelinesStopsDescription => 'Trufis have route stops at final destinations (localities), stops in the downtown  that can be intermediate (downtown streets) or circuits. Interprovincial buses do have fixed stops (Chapare, Tarata, Cliza, Punata, etc.).';
+
+  @override
+  String get guidelinesRouteIdentifiers => 'Route identifiers';
+
+  @override
+  String get layerGroupLeisure => 'Leisure and Entertainment';
+
+  @override
+  String get layerGroupTravel => 'Travel';
+
+  @override
+  String get layerGroupEducation => 'Education';
+
+  @override
+  String get layerGroupHealthServices => 'Health Services';
+
+  @override
+  String get layerAttractionsAndMonuments => 'Attractions and Monuments';
+
+  @override
+  String get layerParksAndSquares => 'Parks and Squares';
+
+  @override
+  String get layerCinemas => 'Cinemas';
+
+  @override
+  String get layerPharmacies => 'Pharmacies';
+
+  @override
+  String get layerHospitals => 'Hospitals';
+
+  @override
+  String get layerSchools => 'Schools';
+
+  @override
+  String get layerInstitutes => 'Institutes';
+
+  @override
+  String get layerShoppingCentersAndSupermarkets => 'Shopping Centers and Supermarkets';
+
+  @override
+  String get layerInterprovincialStops => 'Interprovincial Stops';
+
+  @override
+  String get layerStadiumsAndSportsComplexes => 'Stadiums and Sports Complexes';
+
+  @override
+  String get layerLibraries => 'Libraries';
+
+  @override
+  String get layerTerminalsAndStations => 'Terminals and Stations';
+
+  @override
+  String get layerMuseumsAndArtGalleries => 'Museums and Art Galleries';
+
+  @override
+  String get layerUniversities => 'Universities';
+
+  @override
+  String get guidelinesRouteIdentifiersDescription => 'There are many lines that have variants and for their differentiation they add one more identifier; a colored flag, a different colored panter, a different colored number, an extra letter and the final route inscribed on the panter. For example: 108 with white panter has its destination Av. Humberto Asín bordering with Barrio Entre Rios and 108 Pisiga with yellow panter has its destination in Av. Pisiga.';
+}
